e2c7ed77646aacc3601cd1b4c60c95ef1aaf45c4
Ritika9939 <rk4796327@gmail.com>
2023-04-15 11:14:43 +0530

    demo

---
+++ src/test/java/com/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void test() {
+	  System.out.println("From Test");
+	}
+
+}
